12fd6f3ee2a30403b13e966964abd5f574f68a63
Nurdaulet Zhumantaev <nurdaulet.kaxa@mail.ru>
2026-01-20 14:41:16 +0500

    выполнил пример и свою работу

---
+++ lab1/src/pcBuild.java
@@ -0,0 +1,16 @@
+public class pcBuild {
+
+
+        public static void main(String[] args) {
+            String cpu = "Intel Core i9-13900K";
+            String gpu = "NVIDIA RTX 4090";
+            int ramGb = 64;
+            String ssd = "2TB Samsung 980 Pro";
+
+            System.out.println("💻 КОНФИГУРАЦИЯ СИСТЕМЫ:");
+            System.out.println("Процессор:\t" + cpu);
+            System.out.println("Видеокарта:\t" + gpu);
+            System.out.println("ОЗУ:\t\t" + ramGb + " ГБ");
+            System.out.println("Накопитель:\t" + ssd);
+        }
+    }
