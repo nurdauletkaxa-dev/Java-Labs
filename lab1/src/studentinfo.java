a06db84e8e4fcab62c47acd93cb8e35d0ceb5bc4
Nurdaulet Zhumantaev <nurdaulet.kaxa@mail.ru>
2026-01-20 14:20:14 +0500

    создал studentinfo и добавил туда код

---
+++ lab1/src/studentinfo.java
@@ -0,0 +1,15 @@
+public class studentinfo {
+    public static void main(String[] args) {
+        // Данные студента
+        String fio = "Жумантаев Нурдаулет";
+        String group = "ИС 25-21 ТиПО";
+        String specialty = "Информационные системы";
+
+        // Вывод информации на экран
+        System.out.println("=== Информация о студенте ===");
+        System.out.println("ФИО:         " + fio);
+        System.out.println("Группа:      " + group);
+        System.out.println("Специальность: " + specialty);
+        System.out.println("=============================");
+    }
+}
